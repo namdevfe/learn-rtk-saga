3ed04c1e8ea00b8830e6af9b7ae8ee6824787a7b
namdev <namnkq2603@gmail.com>
2025-02-18 16:57:36 +0700

    #2. Different between takeEvery and takeLastest

--- src/store/reducers/counterSlice.ts
+++ src/store/reducers/counterSlice.ts
@@ -3,10 +3,12 @@ import { RootState } from '..'
 
 interface CounterState {
   value: number
+  loading: boolean
 }
 
 const initialState: CounterState = {
-  value: 0
+  value: 0,
+  loading: false
 }
 
 const counterSlice = createSlice({
@@ -21,12 +23,27 @@ const counterSlice = createSlice({
     },
     increaseByAmount(state, action: PayloadAction<number>) {
       state.value += action.payload
+    },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    increasementSaga(state, action: PayloadAction<number>) {
+      state.loading = true
+    },
+    increasementSuccess(state, action: PayloadAction<number>) {
+      state.value += action.payload
+      state.loading = false
     }
   }
 })
 
 const { actions, reducer: counterReducer } = counterSlice
-export const { increase, decrease, increaseByAmount } = actions
+export const {
+  increase,
+  decrease,
+  increaseByAmount,
+  increasementSaga,
+  increasementSuccess
+} = actions
 export const selectCount = (state: RootState) => state.counter.value
+export const selectCountLoading = (state: RootState) => state.counter.loading
 
 export default counterReducer
